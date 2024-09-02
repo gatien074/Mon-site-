cc5d33dd6bec176ec13905ddb88d7068ed14ba0d
gatien074 <ngouessygatien@gmail.com>
2024-09-02 09:25:51 +0100

    version3

--- app/formations/page.tsx
+++ app/formations/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Header} from '../components/Header';
-import Main from "../components/Main"
+import Main from "../components/main"
 import Formations from "../components/Formations"
 
 
